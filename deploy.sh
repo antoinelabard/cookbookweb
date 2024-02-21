b82a867451386c9007956ecbc2185de839a42ea1
Antoine Labard <antoine@labard.fr>
2024-02-21 21:36:37 +0100

    update website

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Add the canarytokens to each html file
 find recettes -name "*.html" -type f -exec sed -i 's/<div class="markdown-preview-sizer markdown-preview-section">/<div class="markdown-preview-sizer markdown-preview-section kazes"><img src="http:\/\/canarytokens.com\/sw5uvmb82rxizmwhubn225gvr">/g' {} +
 find catégories -name "*.html" -type f -exec sed -i 's/<div class="markdown-preview-sizer markdown-preview-section">/<div class="markdown-preview-sizer markdown-preview-section kazsqd"><img src="http:\/\/canarytokens.com\/t5w0b10daouy2fkd23kgkm6fc">/g' {} +
-find . -name "index.html" -type f -exec sed -i 's/<div class="markdown-preview-sizer markdown-preview-section">/<div class="markdown-preview-sizer markdown-preview-section kxcdw"><img src="http:\/\/canarytokens.com\/ijpluamg2u65kgvhs62jaeykj"></g' {} +
+find . -name "index.html" -type f -exec sed -i 's/<div class="markdown-preview-sizer markdown-preview-section">/<div class="markdown-preview-sizer markdown-preview-section kxcdw"><img src="http:\/\/canarytokens.com\/ijpluamg2u65kgvhs62jaeykj">/g' {} +
 find . -name "menu-de-la-semaine.html" -type f -exec sed -i 's/<div class="markdown-preview-sizer markdown-preview-section">/<div class="markdown-preview-sizer markdown-preview-section ezrdfg"><img src="http:\/\/canarytokens.com\/t5w0b10daouy2fkd23kgkm6fc">/g' {} +
 
 # Display the recipes front-matter
